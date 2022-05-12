1bb58e020ad500f7f2236eabc28b2913c65e3764
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-13 06:41:18 +0700

    added Item model

---
+++ mengukl/cores/src/main/java/com/smktelkommlg/cores/domain/model/Item.kt
@@ -0,0 +1,10 @@
+package com.smktelkommlg.cores.domain.model
+
+data class Item (
+    //TODO: Add properties
+    val id: Int,
+    val name: String,
+    val price: Int,
+    val description: String,
+    val image: String
+)
