7de8957b0a5984ba5c39c2cb4aa457f8c93721a9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-13 06:41:29 +0700

    added show states class

---
+++ mengukl/app/src/main/java/com/smktelkommlg/mengukl/util/ShowStates.kt
@@ -0,0 +1,7 @@
+package com.smktelkommlg.mengukl.util
+
+import com.smktelkommlg.mengukl.databinding.FragmentHomeBinding
+
+interface ShowStates {
+    fun homeLoading(bindingHome : FragmentHomeBinding? = null)
+}
