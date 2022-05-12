67a968fca96cdb3a05322783701e7553561a1909
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-13 06:55:04 +0700

    finished show states

--- mengukl/app/src/main/java/com/smktelkommlg/mengukl/util/ShowStates.kt
+++ mengukl/app/src/main/java/com/smktelkommlg/mengukl/util/ShowStates.kt
@@ -1,7 +1,17 @@
 package com.smktelkommlg.mengukl.util
 
+import android.view.View
 import com.smktelkommlg.mengukl.databinding.FragmentHomeBinding
 
 interface ShowStates {
     fun homeLoading(bindingHome : FragmentHomeBinding? = null)
+    fun homeSuccess(bindingHome : FragmentHomeBinding? = null)
+    fun honeError(bindingHome : FragmentHomeBinding? = null)
+
+    val gone : Int
+        get() = View.GONE
+
+    val visible : Int
+        get() = View.VISIBLE
+
 }
