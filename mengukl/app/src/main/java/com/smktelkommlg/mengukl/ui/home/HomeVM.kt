b63b156b2e368bfecd08fd86d0942c70095d08ca
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-17 06:10:37 +0700

    added home view model

--- mengukl/app/src/main/java/com/smktelkommlg/mengukl/ui/home/HomeVM.kt
+++ mengukl/app/src/main/java/com/smktelkommlg/mengukl/ui/home/HomeVM.kt
@@ -1,7 +1,23 @@
 package com.smktelkommlg.mengukl.ui.home
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.smktelkommlg.cores.data.Resource
+import com.smktelkommlg.cores.domain.model.Item
+import com.smktelkommlg.cores.domain.usecase.ItemUseCase
 
-class HomeVM : ViewModel(){
+class HomeVM(itemUseCase : ItemUseCase) : ViewModel(){
+    private var name : MutableLiveData<String> = MutableLiveData()
+
+    fun setForSearch(query : String){
+        if(name.value == query){
+            return
+        }
+        name.value = query
+    }
+
+    val item : LiveData<Resource<List<Item>>> = Transformations
+        .switchMap(name){
+            itemUseCase.getAllKos(it).asLiveData()
+        }
 
 }
