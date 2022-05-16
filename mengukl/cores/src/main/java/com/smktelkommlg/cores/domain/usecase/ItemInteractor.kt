c3115d20a3f17679753be4b7622e08b9ad5a1092
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-17 06:34:28 +0700

    added ItemInteractor

---
+++ mengukl/cores/src/main/java/com/smktelkommlg/cores/domain/usecase/ItemInteractor.kt
@@ -0,0 +1,16 @@
+package com.smktelkommlg.cores.domain.usecase
+
+import com.smktelkommlg.cores.data.Resource
+import com.smktelkommlg.cores.domain.model.Item
+import com.smktelkommlg.cores.domain.repository.IItemRepository
+import kotlinx.coroutines.flow.Flow
+
+class ItemInteractor (private val itemRepos : IItemRepository) : ItemUseCase{
+    override fun getAllKos(query: String?): Flow<Resource<List<Item>>> {
+        return itemRepos.getAllKos(query)
+    }
+
+    override fun getDetailKos(name: String): Flow<Resource<Item>> {
+        return itemRepos.getDetailKos(name)
+    }
+}
