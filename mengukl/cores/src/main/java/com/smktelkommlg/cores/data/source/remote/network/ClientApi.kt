af59e06ea2259c9e71189f65278a1ddfbbca7403
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-17 07:02:33 +0700

    added Client API

---
+++ mengukl/cores/src/main/java/com/smktelkommlg/cores/data/source/remote/network/ClientApi.kt
@@ -0,0 +1,18 @@
+package com.smktelkommlg.cores.data.source.remote.network
+
+import com.smktelkommlg.cores.data.source.remote.response.ItemResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ClientApi {
+
+    @GET("kosts")
+    suspend fun searchForItem(
+        @Query("q") query: String?
+    ): List<ItemResponse>
+
+    @GET("kosts")
+    suspend fun searchForItemDetail(
+        @Query("q") query: String?
+    ): ItemResponse
+}
