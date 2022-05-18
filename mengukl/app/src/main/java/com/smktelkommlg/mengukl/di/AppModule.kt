0f8dccdc70721a9121caa7311195ded4f1c70ed5
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-18 07:50:37 +0700

    added code for App Module

--- mengukl/app/src/main/java/com/smktelkommlg/mengukl/di/AppModule.kt
+++ mengukl/app/src/main/java/com/smktelkommlg/mengukl/di/AppModule.kt
@@ -1,4 +1,17 @@
 package com.smktelkommlg.mengukl.di
 
-class AppModule {
+import com.smktelkommlg.cores.domain.usecase.ItemInteractor
+import com.smktelkommlg.cores.domain.usecase.ItemUseCase
+import com.smktelkommlg.mengukl.ui.detail.DetailVM
+import com.smktelkommlg.mengukl.ui.home.HomeVM
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module{
+    factory <ItemUseCase>{ItemInteractor(get())}
+}
+
+val viewModelModule = module {
+    viewModel { HomeVM(get()) }
+    viewModel { DetailVM(get()) }
 }
--- mengukl/app/src/main/java/com/smktelkommlg/mengukl/ui/detail/DetailVM.kt
+++ mengukl/app/src/main/java/com/smktelkommlg/mengukl/ui/detail/DetailVM.kt
@@ -1,8 +1,12 @@
 package com.smktelkommlg.mengukl.ui.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.smktelkommlg.cores.domain.usecase.ItemUseCase
 
-class DetailVM : ViewModel() {
+class DetailVM(private val itemUseCase: ItemUseCase) : ViewModel() {
+
+    fun detailItem(name : String) = itemUseCase.getDetailKos(name).asLiveData()
 
 
 }
