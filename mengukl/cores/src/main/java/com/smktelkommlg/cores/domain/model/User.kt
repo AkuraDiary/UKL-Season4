f52ffdf493a8230dab1a4bf9411c52b305e95177
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-10-18 16:34:55 +0700

    added new model of user

---
+++ mengukl/cores/src/main/java/com/smktelkommlg/cores/domain/model/User.kt
@@ -0,0 +1,18 @@
+package com.smktelkommlg.cores.domain.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val id: Int?,
+    val login: String?,
+    val url: String?,
+    val avatarUrl: String?,
+    val name: String?,
+    val location: String?,
+    val type: String?,
+    val publicRepos: Int?,
+    val followers: Int?,
+    val following: Int?
+): Parcelable
